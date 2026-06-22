8e1af4efff00123e8f94ff09fc4d3efa99bba02d
Jakub <kubuskrol@gmail.com>
2026-06-22 20:30:20 +0200

    feat: add Footer component with copyright and social media links

---
+++ nuxt_dashboard/app/components/app/Footer.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const config = useRuntimeConfig();
+</script>
+
+<template>
+	<UFooter :ui="{ top: 'pt-8 pb-0 lg:pt-12 lg:pb-0' }">
+		<template #top>
+			<USeparator :avatar="{ src: '/favicon.svg', alt: config.public.appName || 'Logo' }" />
+		</template>
+
+		<template #left>
+			<p class="text-muted text-sm">
+				Copyright © {{ new Date().getFullYear() }} {{ config.public.appName }}
+			</p>
+		</template>
+
+		<template #right>
+			<UButton
+				icon="i-simple-icons-github"
+				color="neutral"
+				variant="ghost"
+				:to="config.public.gitRepoURL"
+				target="_blank"
+				rel="noopener noreferrer"
+				aria-label="GitHub Repo"
+			/>
+			<UButton
+				icon="i-simple-icons-linkedin"
+				color="neutral"
+				variant="ghost"
+				:to="config.public.linkedInURL"
+				target="_blank"
+				rel="noopener noreferrer"
+				aria-label="LinkedIn"
+			/>
+		</template>
+	</UFooter>
+</template>

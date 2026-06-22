307161d400d6b423e1529d7239d2e65193e95d35
Jakub <kubuskrol@gmail.com>
2026-06-22 20:30:02 +0200

    feat: update Nuxt configuration with app metadata and runtime settings

--- nuxt_dashboard/nuxt.config.ts
+++ nuxt_dashboard/nuxt.config.ts
@@ -1,8 +1,40 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+const appName = "ESPy-Nexus";
+
 export default defineNuxtConfig({
 	modules: ["@nuxt/eslint", "@nuxt/ui"],
 	devtools: { enabled: true },
-	compatibilityDate: "2025-07-15",
+
+	app: {
+		head: {
+			title: appName,
+			titleTemplate: "%s",
+
+			htmlAttrs: {
+				lang: "pl",
+			},
+			meta: [
+				{ name: "apple-mobile-web-app-title", content: appName },
+			],
+			link: [
+				{ rel: "icon", type: "image/png", sizes: "96x96", href: "/favicon-96x96.png?v=20260622" },
+				{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg?v=20260622" },
+				{ rel: "shortcut icon", href: "/favicon.ico?v=20260622" },
+				{ rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png?v=20260622" },
+				{ rel: "manifest", href: "/site.webmanifest?v=20260622" },
+			],
+		},
+	},
+	css: ["~/assets/css/main.css"],
+
+	runtimeConfig: {
+		public: {
+			appName: appName,
+			gitURL: "https://github.com/Niewiaro",
+			gitRepoURL: "https://github.com/Niewiaro/ESPy-Nexus",
+			linkedInURL: "https://www.linkedin.com/in/jakub-niewiarowski",
+		},
+	},
+	compatibilityDate: "2026-06-22",
 
 	eslint: {
 		config: {

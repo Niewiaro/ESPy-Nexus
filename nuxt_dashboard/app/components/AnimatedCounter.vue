829805a6afe8bc5b44521888a10dc8962f4d50a8
Jakub <kubuskrol@gmail.com>
2026-07-01 19:42:53 +0200

    feat: add AnimatedCounter component for animated number display

---
+++ nuxt_dashboard/app/components/AnimatedCounter.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useTransition, TransitionPresets } from "@vueuse/core";
+
+const props = defineProps<{
+	value: number;
+}>();
+
+const formatNumber = (num: number): string => {
+	return new Intl.NumberFormat("pl-PL").format(Math.floor(num));
+};
+
+const source = ref(0);
+const output = useTransition(source, {
+	duration: 1500,
+	transition: TransitionPresets.easeOutCubic,
+});
+
+onMounted(() => {
+	source.value = props.value;
+});
+
+const displayValue = computed(() => formatNumber(Math.floor(output.value)));
+</script>
+
+<template>
+	<span>{{ displayValue }}</span>
+</template>
